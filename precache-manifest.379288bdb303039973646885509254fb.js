e545cb5caf5f815d89d6ea325ede9aaf313e2830
Anjr0o <84.andrew.stokes@gmail.com>
2020-10-02 00:20:38 -0500

    Updates

---
+++ precache-manifest.379288bdb303039973646885509254fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c252a457b930e67e38002251ebeeec41",
+    "url": "/winterbook/index.html"
+  },
+  {
+    "revision": "50d303aa3b02527c31d6",
+    "url": "/winterbook/static/css/main.260cdc60.chunk.css"
+  },
+  {
+    "revision": "47886dc38ae5840f244e",
+    "url": "/winterbook/static/js/2.dcc31892.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/winterbook/static/js/2.dcc31892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d303aa3b02527c31d6",
+    "url": "/winterbook/static/js/main.fe7a5120.chunk.js"
+  },
+  {
+    "revision": "cbaadd872b01651f5f0e",
+    "url": "/winterbook/static/js/runtime-main.99b6412d.js"
+  }
+]);
